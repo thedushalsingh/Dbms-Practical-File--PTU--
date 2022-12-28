01496a6438af660feda8bc4f48844d449295c3b8
Dushal Singh <120463747+creatordushal@users.noreply.github.com>
2022-12-28 23:30:26 +0530

    Add files via upload

---
+++ New Library/Book.sql
@@ -0,0 +1,6 @@
+Create Table Book(
+Book_id varchar(20) primary key, 
+Title varchar(20), 
+Publisher_Name varchar(25), 
+Pub_Year date
+);
---
+++ New Library/Publisher.sql
@@ -0,0 +1,5 @@
+Create Table PUBLISHER (
+Name varchar(25) Primary Key , 
+Address varchar(50), 
+Phone double
+);
